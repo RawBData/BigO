51a8cb0470fd04f3cb754be2e2534da96f022bfd
RawBData <lunarmonky@yahoo.com>
2020-01-24 11:51:49 -0800

    jquery snippets

---
+++ jquery_snippets.js
@@ -0,0 +1,33 @@
+$.(".Fw(600)")
+
+$(document).ready(function(){
+
+})
+
+dir($(".classname"))
+for (i=0; i<newArr.length; i++){console.log(newArr[i].innerHTML)}
+$("a")[52]
+$(document).ready(function(){
+    console.log("shite")
+})
+
+
+//First injec this code into the head by adding in console
+var jQueryScript = document.createElement('script');  
+jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+document.head.appendChild(jQueryScript);
+
+//set variables to manipulate
+let newArr;
+let list;
+
+//Then get the range of elements
+newArr = $("a").slice(49,66)
+
+list = "";
+for (i=0; i<newArr.length; i++){
+    list += newArr[i].innerHTML + ",";
+}
+console.log(list);
+
+//EHTH VIR ADPT TEAM INTC BLDP CSPCY PAAS TXG PTBRY AU AG
