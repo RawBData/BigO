2f1d36e359c86cf7f3fd45211d7e3e542900ba45
RawBData <lunarmonky@yahoo.com>
2020-01-28 17:20:30 -0800

    finish snipppet

--- jquery_snippets.js
+++ jquery_snippets.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     console.log("shite")
 })
 
-
+//1st snippet
 //First injec this code into the head by adding in console
 var jQueryScript = document.createElement('script');  
 jQueryScript.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
@@ -24,14 +24,22 @@ document.head.appendChild(jQueryScript);
 //set variables to manipulate
 let newArr;
 let list;
+let SearchAddress;
 
+
+//2nd Snippet
 //Then get the range of elements
-newArr = $("a").slice(49,66)
+newArr = $("a").slice(50,75)
 
 list = "";
 for (i=0; i<newArr.length; i++){
     list += newArr[i].innerHTML + ",";
 }
 console.log(list);
+//https://www.stockcharts.com/freecharts/candleglance.html?VIR,KKPNF,VIE,DNLI,GSX,WIMHF,MRNA,NVRO|D|0
+
+SearchAddress = "https://www.stockcharts.com/freecharts/candleglance.html?" + list + "|D|0";
+//clickable link for stock details
+console.log(SearchAddress);
 
 //EHTH VIR ADPT TEAM INTC BLDP CSPCY PAAS TXG PTBRY AU AG
