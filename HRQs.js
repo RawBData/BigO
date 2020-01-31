e9909c0777931425b74319cdb6fdbe03c2986bd4
RawBData <lunarmonky@yahoo.com>
2020-01-30 16:32:43 -0800

    finish problem and set up environment for future problems

--- HRQs.js
+++ HRQs.js
@@ -1,28 +1,62 @@
+/*
+Below are Hack Rank Questions that I have solved
+
+*/
+
+/*
+
+//the first code below is just a snippet I created to run the hacker rank questions from terminal locally.
+
 'use strict';
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
+rl.question("What is the string? ", function(s) {
+    rl.question("which letter to count? ", function(l) {
+        //
+        // console.log(`string = ${s}, n = ${l}`);
+        console.log(someFunction(s, l));
+        rl.close();
+    });
 });
 
-function readLine() {
-    return inputString[currentLine++];
+rl.on("close", function() {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
+
+function someFunction(s,n){
+    console.log(s,n);
 }
+*/
+
+
+
+'use strict';
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is the string? ", function(s) {
+    rl.question("how many letters to count? ", function(n) {
+        console.log(repeatedString(s, n)) 
+        // console.log(`${name}, is a citizen of ${country}`);
+
+        rl.close();
+    });
+});
+
+rl.on("close", function() {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+});
 
 // Complete the repeatedString function below.
 function repeatedString(s, n) {
@@ -41,7 +75,7 @@ function repeatedString(s, n) {
     let numExtraLetters = (n%s.length);
     for(let i=0;i<s.length;i++){
         let letter = s[i];
-        console.log(s);
+        // console.log(s);
         if(letter==='a'){
             count += 1;
             if(i<numExtraLetters){
@@ -55,16 +89,4 @@ function repeatedString(s, n) {
     return allCount;
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const s = readLine();
-
-    const n = parseInt(readLine(), 10);
-
-    let result = repeatedString(s, n);
-
-    ws.write(result + "\n");
-
-    ws.end();
-}
