3421c76460653983ff4edf2f3555bf4545790ca1
RawBData <lunarmonky@yahoo.com>
2020-02-02 12:29:35 -0800

    more of that

--- HRQs.js
+++ HRQs.js
@@ -151,4 +151,6 @@ function checkMagazine(magazine, note) {
     }
 
     console.log(Object.values(noteHash).every( x => x === 0)?"Yes":"No")
+    console.log(Object.values(noteHash).every( x => x === 0)?"Yes":"No")
+    console.log(Object.values(noteHash).every( x => x === 0)?"Yes":"No")
 }
