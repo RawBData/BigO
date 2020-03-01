c446fc25da8a6dd4775a0ad1066f33c3ce3e8b2d
RawBData <lunarmonky@yahoo.com>
2020-02-29 22:53:00 -0800

    add explanation

--- HRQs.js
+++ HRQs.js
@@ -139,6 +139,7 @@ function jumpingOnClouds(c) {
 //Find optimized way to check of every item from one array is in another
 function checkMagazine(magazine, note) {
     //create the empty notehash
+    //and then load hash with letters
     let noteHash = {};
 
     let noteArr = note;
