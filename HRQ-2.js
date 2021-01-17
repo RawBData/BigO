c306f17b9002628295cc760d3c790aaee7a0f256
RawBData <lunarmonky@yahoo.com>
2021-01-17 12:01:00 -0800

    example 1

--- HRQ-2.js
+++ HRQ-2.js
@@ -2,6 +2,7 @@
 Below is a group of problems from memoization chapter
 Also various questions from hacker rank
 with examples and solutions
+first exmample findding pairs
 */
 
 // function countTriplets(arr, r) {
