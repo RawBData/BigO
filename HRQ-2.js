6e58e9994acfe9a92aa4fb1ae9582ccba81bcbba
RawBData <lunarmonky@yahoo.com>
2021-01-26 20:44:22 -0800

    more details

--- HRQ-2.js
+++ HRQ-2.js
@@ -162,6 +162,7 @@ const bestSum = (targetNum,numbers,memo={})=>{
 console.log(bestSum(7,[2,3]));
 console.log(bestSum(7,[5,3,4,7]));
 console.log(bestSum(8,[2,3,5]));
+console.log(bestSum(10,[2,3,5]));
 console.log(bestSum(8,[1,4,5]));
 console.log(bestSum(100,[1,2,5,25]));
 
