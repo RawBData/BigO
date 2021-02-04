da14dd3cc540c6a4f014ed16081a8e8464b2bf49
RawBData <lunarmonky@yahoo.com>
2021-02-03 20:40:28 -0800

    bestsum ex

--- HRQ-2.js
+++ HRQ-2.js
@@ -160,6 +160,7 @@ const bestSum = (targetNum,numbers,memo={})=>{
 }
 
 console.log(bestSum(7,[2,3]));
+console.log(bestSum(7,[1,8]));
 console.log(bestSum(7,[5,3,4,7]));
 console.log(bestSum(8,[2,3,5]));
 console.log(bestSum(10,[2,3,5]));
