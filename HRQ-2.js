1248ff8c808bc980d9a5ba165555dea4f7167a93
RawBData <lunarmonky@yahoo.com>
2021-03-24 20:16:38 -0700

    examples

--- HRQ-2.js
+++ HRQ-2.js
@@ -39,6 +39,7 @@ and preparation
 
 // // console.log(countTriplets(new Array(100).fill(1),1));
 // // console.log(countTriplets([1,5,5,25,125],5));
+// console.log(countTriplets([1,6,7,5,125],4));
 // console.log(countTriplets([1,2,1,2,4],2));
 // 0 1 1 2 3 5 8 13 21 39
 // 0 1 1 2 3 5 8 13 21 39
